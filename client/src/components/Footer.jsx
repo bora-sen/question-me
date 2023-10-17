ea83815542483703671cef193cbcfecfeb035d05
bora-sen <bahattinborasen@gmail.com>
2023-10-17 17:27:39 +0300

    +added Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     <footer className="py-3 my-4">
       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
         <li className="nav-item">
-          <a href="#" className="nav-link px-2 text-body-secondary">
+          <a href="/create-test" className="nav-link px-2 text-body-secondary">
             Create a Test
           </a>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link px-2 text-body-secondary">
+          <a href="/solve-test" className="nav-link px-2 text-body-secondary">
             Solve a Test
           </a>
         </li>
