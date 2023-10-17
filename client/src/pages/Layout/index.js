ed0d2017c515b78743720fdd032684eacfe7ef25
bora-sen <bahattinborasen@gmail.com>
2023-10-17 08:24:57 +0300

    Switched To Vite

--- client/src/pages/Layout/index.js
+++
@@ -1,2 +0,0 @@
-export {default as Header} from './Header/index.jsx';
-export {default as Footer} from './Footer/index.jsx';
--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export {default as CreateTestPage} from './CreateTestPage/index.jsx';
-export {default as LandingPage} from './LandingPage/index.jsx';
-export {default as SolveSpesificTest} from './SolveSpesificTest';
-export {default as SolveTestPage} from './SolveTestPage';
-export {default as NotFoundPage} from './404/index';
