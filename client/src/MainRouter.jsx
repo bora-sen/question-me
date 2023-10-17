fea7e19290ae2457de1852dde1490ad3b580d337
bora-sen <bahattinborasen@gmail.com>
2023-10-17 17:28:25 +0300

    +added new routes

--- client/src/MainRouter.jsx
+++ client/src/MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import { CreateTest, Landing } from "./pages"
+import { CreateTest, Landing, Result, SolveTest, SolveTestLink } from "./pages"
 import { Header, Footer } from "./components"
 
 function MainRouter() {
@@ -10,6 +10,9 @@ function MainRouter() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/create-test" element={<CreateTest />} />
+        <Route path="/solve-test/:accessLink/:name" element={<SolveTestLink />} />
+        <Route path="/solve-test" element={<SolveTest />} />
+        <Route path="/get-result/:accessLink" element={<Result />} />
       </Routes>
       <Footer />
     </>
